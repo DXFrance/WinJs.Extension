7fba03646db0e7f48168fdb1f1af243499551f78
David Rousset <davrous@microsoft.com>
2012-07-24 13:46:28 +0200

    Creation de la 1ere structure
    répertoires css, js, strings (pour loca?) et définition du namespace via
    WinJS.

---
+++ WinJS.Extension.Core/js/WinJS.Extension.Core.js
@@ -0,0 +1,14 @@
+﻿/*!
+  Library build by Pierre Lagarde & David Rousset from Microsoft France
+  This library is supported for use in Windows Metro Style Apps only.
+*/
+/// <reference path="ms-appx://Microsoft.WinJS.1.0.RC/js/base.js" />
+/// <reference path="ms-appx://Microsoft.WinJS.1.0.RC/js/ui.js" />
+
+(function () {
+    "use strict";
+
+    // Exports
+    WinJS.Namespace.define("WinJS.Extension.Core");
+
+})();
