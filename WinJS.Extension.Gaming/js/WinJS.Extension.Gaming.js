6a0137c3267b82132bcd7fa3834d7846f18895f0
David Rousset <davrous@microsoft.com>
2012-07-24 19:51:51 +0200

    First Draft of Gaming.Setting

--- WinJS.Extension.Gaming/js/WinJS.Extension.Gaming.js
+++ WinJS.Extension.Gaming/js/WinJS.Extension.Gaming.js
@@ -11,4 +11,76 @@
     // Exports
     WinJS.Namespace.define("WinJS.Extension.Gaming");
 
+    // export this "enum"
+    var SettingsType = Object.freeze({
+        None: 0,
+        Sounds: 1,
+        Inputs: 2,
+        About: 3
+    });
+
+    WinJS.Namespace.define("WinJS.Extension.Gaming", {
+        SettingsType: SettingsType
+    });
+
+    function BindSettings(settingsArray) {
+        var i;
+        var commands = {};
+        for (i = 0; i < settingsArray.length; i++) {
+            var commandsDetails = {};
+            commandsDetails.href = settingsArray[i]._href;
+            commandsDetails.title = settingsArray[i]._title;
+            commands[settingsArray[i]._targetedDiv] = commandsDetails;
+        }
+        WinJS.Application.onsettings = function (e) {
+            e.detail.applicationcommands = commands;
+            WinJS.UI.SettingsFlyout.populateSettings(e);
+        }
+    };
+
+    WinJS.Namespace.define("WinJS.Extension.Gaming", {
+        BindSettings: BindSettings
+    });
+
+    // constructor function
+    function initSettings(type) {
+        if (type) {
+            this.type = type;
+            switch (type) {
+                case WinJS.Extension.Gaming.SettingsType.Sounds:
+                    this._href = "/WinJS.Extension.Gaming/html/SoundsUI.html";
+                    this._title = "Sounds & Music";
+                    this._targetedDiv = "soundsDiv";
+                    break;
+                case WinJS.Extension.Gaming.SettingsType.Inputs:
+                    this._href = "/WinJS.Extension.Gaming/html/InputsUI.html";
+                    this._title = "Inputs";
+                    this._targetedDiv = "inputsDiv";
+                    break;
+                case WinJS.Extension.Gaming.SettingsType.About:
+                    this._href = "/WinJS.Extension.Gaming/html/AboutUI.html";
+                    this._title = "About";
+                    this._targetedDiv = "aboutDiv";
+                    break;
+            }
+        }
+    }
+
+    var instanceMembers = {
+        type: WinJS.Extension.Gaming.None,
+        _href: "",
+        _title: "",
+        _targetedDiv: "",
+    };
+
+    var staticMembers = {
+    };
+
+    var Setting = WinJS.Class.define(initSettings, instanceMembers, staticMembers);
+
+    // add to namespace
+    WinJS.Namespace.define("WinJS.Extension.Gaming", {
+        Setting: Setting
+    });
+
 })();
